86f63aadd9a540b9ce74a8a597637217b3a3bebf
James Beal <James.Beal@sanger.ac.uk>
2016-03-15 15:03:46 +0000

    Try with a shared filesystem

--- add_compute.sh
+++ add_compute.sh
@@ -9,15 +9,14 @@ fi
 sed -i -e 's/127.0.0.1 localhost//' -e 's/127.0.1.1/127.0.0.1/' /etc/cloud/templates/hosts.debian.tmpl 
 echo "$SERVER_IP   $SERVER_NAME" >> /etc/hosts
 echo "$SERVER_IP   $SERVER_NAME" >> /etc/cloud/templates/hosts.debian.tmpl
-sed -i "s/localhost/$SERVER_NAME/" /opt/openlava-3.1/etc/lsb.hosts
-sed -i "s/localhost/$SERVER_NAME !  !     1       -       -\n`hostname`/" /opt/openlava-3.1/etc/lsf.cluster.openlava
-echo LSF_SERVER_HOSTS=\"$SERVER_NAME\" >> /opt/openlava-3.1/etc/lsf.conf 
+sed -i "s/End     Host/`hostname` !  !     1       -       -\nEnd     Host/" /opt/openlava-3.1/etc/lsf.cluster.openlava
+echo "$SERVER_NAME:/home   /home  nfs defaults 0 0" >> /etc/fstab
+echo "$SERVER_NAME:/opt/openlava-3.1 /opt/openlava-3.1   nfs defaults 0 0" >> /etc/fstab
+mount /home
+mount /opt/openlava-3.1 
+sed -i "s/End     Host/`hostname` !  !     1       -       -\nEnd     Host/" /opt/openlava-3.1/etc/lsf.cluster.openlava
 ln -s /opt/openlava-3.1/etc/openlava /etc/init.d
 update-rc.d openlava defaults
-chmod -R 700 /root/.ssh /opt/openlava-3.1/work
-chown -R root /root/.ssh /opt/openlava-3.1/work
-echo "$SERVER_NAME:/home   /home  nfs defaults 0 0" >> /etc/fstab
-mount /home
 (ip addr show dev eth0 | grep "inet " | awk '{print $2}' | awk -F/ '{printf ("%s ",$1)}' ; hostname ) |  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@$SERVER_NAME 'sudo tee -a /etc/hosts'
 (ip addr show dev eth0 | grep "inet " | awk '{print $2}' | awk -F/ '{printf ("%s ",$1)}' ; hostname ) |  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@$SERVER_NAME 'sudo tee -a /etc/cloud/templates/hosts.debian.tmpl'
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@$SERVER_NAME "sudo lsaddhost  `hostname`"
