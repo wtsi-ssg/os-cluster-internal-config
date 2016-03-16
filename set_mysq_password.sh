b111315cb02375d1ac55202614a486700fbc39ea
James Beal <James.Beal@sanger.ac.uk>
2016-03-16 14:37:31 +0000

    Set non interactive

--- set_mysq_password.sh
+++ set_mysq_password.sh
@@ -3,12 +3,12 @@ logger  "reseting mysql password"
 if [ -f /home/ubuntu/.my.cnf ] ; then
   exit 0
 fi
+export DEBIAN_FRONTEND=noninteractive
 apt-get install pwgen
 NEW_PW=`pwgen 8 1`
 mysqladmin -u root -psupersecret password $NEW_PW
 cat << EOF > /home/ubuntu/.my.cnf
 [client] 
-socket=/tmp/mysql.sock
 user = root
 password = $NEW_PW
 EOF
