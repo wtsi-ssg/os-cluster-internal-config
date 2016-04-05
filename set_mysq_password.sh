6983926f0b48d6c47514280eea3656199b4801a1
James Beal <james.beal@sanger.ac.uk>
2016-04-05 22:14:53 +0100

    More failsafes, and a reboot to restart

--- set_mysq_password.sh
+++ set_mysq_password.sh
@@ -8,6 +8,7 @@ apt-get install pwgen
 NEW_PW=`pwgen -1cns`
 mysqladmin -u root -psupersecret password $NEW_PW
 cat << EOF > /home/ubuntu/.my.cnf
+# Do not delete this file.
 [client] 
 user = root
 password = $NEW_PW
@@ -17,6 +18,9 @@ logger  "mysql password reset complete"
 /etc/init.d/mysql stop
 grep -q  /data01 /proc/mounts
 if [ $? -eq 0 ] ; then
+  if [ -d /data01/mysql_server ] ; then
+    exit 0
+  fi
   mkdir /data01/mysql_server
   ( cd /var/lib/; tar cf -  mysql ) | ( cd /data01/mysql_server/ ; tar xfp -)
   mv /var/lib/mysql /var/lib/mysql.old
@@ -30,4 +34,5 @@ fi
 # 512MB for the OS and then 80% of the rest
 INNODB=`cat /proc/meminfo | awk '/MemTotal:/ {printf("%d", .8*(($2-(512*1024)))/1024)}'`
 sed -e 's/^# . InnoDB.*$/# \* InnoDB \ninnodb_buffer_pool_size = '$INNODB'M/'  -i /etc/mysql/my.cnf 
-/etc/init.d/mysql start
+sleep 10 
+reboot
